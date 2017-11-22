9747f73f32848078f2e7b8862cb326b6d290203c
Peleg Rosenthal <peleg3@gmail.com>
2017-11-22 18:53:45 -0500

    get email from site if possible

--- get_saved.php
+++ get_saved.php
@@ -25,6 +25,34 @@ function getSavedUsernames($ig, $maxId = false) {
   return $usernames;
 }
 
+function tryGetEmail($webUrl, $bio) {
+  $emailRegex = '/([0-9a-z_\.]+@[0-9a-z_\.]+)/i';
+
+  if (preg_match($emailRegex, $webUrl, $matches)) { // url IS email
+    return $matches[0];
+  }
+
+  if (preg_match($emailRegex, $bio, $matches)) { // bio HAS email
+    return $matches[0];
+  }
+
+  if ($webUrl) {
+    // crawl site. hope for the best
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $webUrl);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); 
+    curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1; .NET CLR 1.0.3705; .NET CLR 1.1.4322)');
+    try {
+      $html = curl_exec($ch);
+      if (preg_match($emailRegex, $html, $matches)) { // site has email
+        return $matches[0];
+      }
+    } catch(Exception $e) { /* dont care */ }
+  }
+
+  return '';
+}
+
 $username = 'brizoloves';
 $password = getenv('BRIZO_INSTA_PASSWORD');
 $ig = new \InstagramAPI\Instagram();
@@ -33,7 +61,7 @@ $ig->login($username, $password);
 $uniqueUsernames = array_unique(getSavedUsernames($ig));
 
 $fp = fopen('./out/users.csv', 'w');
-fputcsv($fp, ['NAME', 'USERNAME', 'FOLLOWERS', 'URL', 'BIO']);
+fputcsv($fp, ['NAME', 'USERNAME', 'FOLLOWERS', 'EMAIL', 'URL', 'BIO']);
 
 foreach($uniqueUsernames as $username) {
   $user = $ig->people->getInfoByName($username)->user;
@@ -41,6 +69,7 @@ foreach($uniqueUsernames as $username) {
     $user->full_name,
     $user->username,
     $user->follower_count,
+    tryGetEmail($user->external_url, $user->biography),
     $user->external_url,
     $user->biography
   ]);
